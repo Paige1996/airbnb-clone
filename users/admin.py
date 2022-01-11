25f45ef6381c87a71e5f56cf736bef242492b022
이은주 <eunjoo@ieunjus-MacBook-Pro.local>
2022-01-11 20:29:10 +0900

    making rooms, users applications in models, admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+# * Admin : admin.py에서 model을 가져오려면 @admin register(models.User)..
+# 즉 decoratro을 class 와 함께 해줘야함.  models.User를  Class CustomUserAdmin(UserAdmin)라는 클래스에 적용하겠다는 뜻.  @admin register(models.User)이거 대신 admin.site.register(models.User, CustomUserAdmin) 써도됨. 이것도 동일한 말.
+# https://docs.djangoproject.com/en/2.2/ref/contrib/admin/
+@admin.register(models.User)  # decorator. admin패널에서 이 user을 보고싶어
+class CustomUserAdmin(UserAdmin):  # customUserAdmin으로 user을 컨트롤 UserAdmin은 상속받음
+
+    """Custom User Admin"""
+
+    fieldsets = (
+        UserAdmin.fieldsets
+        + (  # userAdmin은 이미 장고가 자동으로 가지고 있다. 그거에다가 filedset이라는 내가만든 세트도 같이 화면에 적용해달라는것임
+            (
+                "Custom profile",  # 필드의 제목
+                {
+                    "fields": (  # 필드 내용
+                        "avatar",
+                        "gender",
+                        "bio",
+                        "birthdate",
+                        "language",
+                        "currency",
+                        "superhost",
+                    )
+                },
+            ),
+        )
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import DateField
+from django.utils.translation import LANGUAGE_SESSION_KEY
 
 # Create your models here.
+
+
+class User(AbstractUser):  # 상속 받음. base.py안에있는model으로부터 그리고 AbstracUser로 받았음.
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "male"),
+        (GENDER_FEMALE, "female"),
+        (GENDER_OTHER, "other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "EN"),
+        (LANGUAGE_KOREAN, "KR"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
+    avatar = models.ImageField(
+        blank=True
+    )  # null은 데이터베이스에 쓰이는 거고 blanks는 form에 적용된느거. 값이 없어도 된다는 뜻.
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
